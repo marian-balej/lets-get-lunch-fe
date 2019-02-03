db20c28b6db158b8ad05c22a8dd8309d08ebd3aa
marian.balej <marian.balej@hotmail.com>
2019-02-03 23:55:41 +0100

    Add user signup

---
+++ src/app/services/auth/auth.service.spec.ts
@@ -0,0 +1,88 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+  } from '@angular/common/http/testing';
+
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+import { of } from 'rxjs';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let http: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService]
+    });
+    authService = TestBed.get(AuthService); 
+    http = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(authService).toBeTruthy();
+  });
+
+  describe('signup', () => {
+    it('should return a token with a valid username and password', () => {
+      const user = {'username': 'myUser', 'password': 'password'};
+      const signupResponse = {
+        '__v': 0,
+	'username': 'myUser',
+	'password': 'blabla',
+	'_id': 'blabla',
+	'dietPreferences': []
+	}
+	const loginResponse = { 'token': 's3cr3tt0ken' };
+      let response;
+
+      authService.signup(user).subscribe(res => {
+        response = res;
+      });
+			spyOn(authService, 'login').and.callFake(() => of(loginResponse));
+
+    http.expectOne('http://localhost:8080/api/users').flush(signupResponse);
+    expect(response).toEqual(loginResponse);
+    http.verify();
+    });
+
+  it ('should return an error for an invalid user object', () => {
+  	const user = {username: 'myUser', password: 'pswd' };
+	const signupResponse = 'Your password must be at least 5 characters long.';
+	let errorResponse;
+
+	authService.signup(user).subscribe(res => {}, err => {
+		errorResponse = err;
+		});
+	
+	http
+		.expectOne('http://localhost:8080/api/users')
+		.flush({message: signupResponse}, {status: 400, statusText: 'Bad Request'});
+
+	expect(errorResponse.error.message).toEqual(signupResponse);
+	http.verify();
+  });
+
+  });  
+
+
+	describe('login', () => {
+		it('should return a token with a valid username and password', () => {
+			const user = { 'username': 'myUser', 'password': 'password'};
+			const loginResponse = { 'token': 's3cr3tt0ken' };
+			let response;
+
+			authService.login(user).subscribe(res => {
+				response = res;
+			});
+
+			http.expectOne('http://localhost:8080/api/sessions').flush(loginResponse);
+			expect(response).toEqual(loginResponse);
+			expect(localStorage.getItem('Authorization')).toEqual('s3cr3tt0ken');
+			http.verify();
+		});
+	});
+
+});
